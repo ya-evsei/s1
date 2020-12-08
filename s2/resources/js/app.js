59e8782fcdb456b4773cf681214ac409b108e88b
Ya-evsei <ya.evsei@gmail.com>
2020-12-08 14:17:01 +0300

    start vue use

--- s2/resources/js/app.js
+++ s2/resources/js/app.js
@@ -1,3 +1,14 @@
-require('./bootstrap');
+import Vue from 'vue'
+import {
+	BootstrapVue,
+	IconsPlugin
+} from 'bootstrap-vue'
 
-require('alpinejs');
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+	// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+new Vue({
+	render: h => h(App),
+}).$mount('#app')
